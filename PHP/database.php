b123770a2cb5af3554602c5459f1f5cc8933d797
sabrinasabat <sabrinasabats@gmail.com>
2024-11-01 13:48:53 +0100

    conexion ddbb

---
+++ PHP/database.php
@@ -0,0 +1,18 @@
+<?php
+
+//Datos de la conexión:
+    $host = 'sql108.infinityfree.com';
+    $dbname = 'if0_37620352_prova';
+    $username = 'if0_37620352';
+    $password = 'swagnabrisa1';
+
+//Crear una nueva conexión:
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+//Verificar la conexión:
+    if($conn->connect_error){
+        die("No fue posible conectar: ".$conn->connect_error);
+    }
+    echo "Conexión exitosa";
+
+?>
