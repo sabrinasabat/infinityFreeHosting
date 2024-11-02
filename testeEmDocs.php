e3046c592bcee459b3a2c7b3ca8ebc7e8a1cd6ad
sabrinasabat <sabrinasabats@gmail.com>
2024-11-02 14:23:48 +0100

    teste commit

---
+++ testeEmDocs.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Página de teste diretamente em htdocs</h1>
+        <?php
+        echo "Teste PHP diretamente em htdocs";
+        ?>
+</body>
+</html>
