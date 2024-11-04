febaee6c8778a72722a471e0d19c6debdfdbd5b2
sabrinasabat <sabrinasabats@gmail.com>
2024-11-04 23:54:34 +0100

    commit 04.11.24 23:54

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ function descobrirNavegador($navegador){
 
     $version="";
     $versionActual = "";
-    if (preg_match('/Edg/i',$navegador)) {
-        preg_match('/Edg\/([\d\.]+)/i',$navegador,$version);
-        $versionActual = $version[1];
+    if (preg_match('/Edg/i',$navegador)) { /*Verificando si la String obtenida en 'HTTP_USER_AGENT' contiene
+las palabras 'EDG'. /i ignora la diferencia de maiuscula y minuscula*/
+        preg_match('/Edg\/([\d\.]+)/i',$navegador,$version); /*Para capturar la version en formato
+ tipico de versiones. Números y puntos*/
+        $versionActual = $version[1];//He pillado el primer grupo capturado, el que corresponde solamente a la version
         return 'Tu navegador es el: EDGE.'."<br><br>".'La versión actual es: '.$versionActual;
     } else if (preg_match('/Chrome/i',$navegador)) {
         preg_match('/Chrome\/([\d\.]+)/i',$navegador,$version);
