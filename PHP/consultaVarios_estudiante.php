041c97bb6ec9a436bf0e0cee45b52ce3f82f66f8
sabrinasabat <sabrinasabats@gmail.com>
2024-11-07 10:04:27 +0100

    commit 07.11.24 10:04

--- PHP/consultaVarios_estudiante.php
+++ PHP/consultaVarios_estudiante.php
@@ -12,13 +12,14 @@
             $edad = $fila['edad'];
             $ciclo = $fila['ciclo'];
 
+            $fila = mysqli_fetch_assoc($res);
             echo "<tr>";
             echo "<td>".$dni."</td>";
             echo "<td>".$nombre."</td>";
             echo "<td>".$edad."</td>";
             echo "<td>".$ciclo."</td>";
             //echo "Nombre: ".$nombre."<br>Edad: ".$edad."<br>";
-            $fila = mysqli_fetch_assoc($res);
+
         }
     }
     mysqli_free_result($res);
