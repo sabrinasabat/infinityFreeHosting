7c7fdb1196b9c35f28f07dab62db77c5bba96c67
sabrinasabat <sabrinasabats@gmail.com>
2024-11-13 10:52:52 +0100

    Commit 13.11.24 10:52

--- JavaScript/u3e1_fincurso.js
+++ JavaScript/u3e1_fincurso.js
@@ -1,14 +1,8 @@
-function finCurso() {
-
-    var inicioCurso = new Date();
-
-    var finCurso = new Date(2025, 5, 24);
-
-    var diferencia = finCurso-inicioCurso;
-
+function finAno() {
+    var fecha = new Date();
+    var finAno = new Date(2024, 11, 31);
+    var diferencia = finAno-fecha;
     var diasRestantes = Math.ceil(diferencia / (1000 * 60 * 60 * 24));
-
-
-    window.alert("faltan: "+ diasRestantes+ " días para el fin de curso :) ")
-
+    window.alert("faltan: "+ diasRestantes+ " días para el fin del año :) ")
 }
+finAno();
