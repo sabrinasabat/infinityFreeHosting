217ee00876ba62a3b12fd4a9ddb1ae3e0fb59cd7
sabrinasabat <sabrinasabats@gmail.com>
2024-11-21 09:31:20 +0100

    commit 21.11.24 09:31

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -7,9 +7,12 @@
     }
 
     $dadesJason = json_decode($dades, true);
+    $dadesJ = json_encode($dadesJason, JSON_PRETTY_PRINT);
 
     echo "Nombre: $nom <br>";
     echo "DNI: $dni <br>";
     echo "Edad: $edat <br>";
     echo "Dades: $dadesJason<br>;"
+
+
 ?>
