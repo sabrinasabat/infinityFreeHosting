0a57adbffb1d027402af4e66ba411257ba275bab
sabrinasabat <sabrinasabats@gmail.com>
2024-11-21 08:56:51 +0100

    commit 21.11.24 08:56

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -1,5 +1,5 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"]=="GET"){
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
         $nom = $_GET['nom'];
         $dni = $_GET['dni'];
         $edat = $_GET['edat'];
