1b5b81a4115bb42b3094885a80d3e96c4b1bb053
sabrinasabat <sabrinasabats@gmail.com>
2024-11-21 10:07:25 +0100

    commit 21.11.24 10:07

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -9,14 +9,12 @@
 
     // Intentamos decodificar el campo JSON (dades)
     $dades_json = json_decode($dades, true); // Convertirlo en un array asociativo
+    $dades_json.json_encode($dades_json, JSON_PRETTY_PRINT);
 
-    $dades_display = json_encode($dades_json, JSON_PRETTY_PRINT); // Mostrar el JSON formateado
 
-
-    // Mostrar los datos procesados
     echo "Nombre: $nom <br>";
     echo "DNI: $dni <br>";
     echo "Edad: $edat <br>";
-    echo "Dades (JSON): <pre>$dades_display</pre><br>";
+    echo "Dades: <pre>$dades_json</pre><br>";
 
 ?>
