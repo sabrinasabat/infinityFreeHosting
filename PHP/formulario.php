0b4a445fab5f5c1e4b1fa4c6d8764b241e1bbe7c
sabrinasabat <sabrinasabats@gmail.com>
2024-11-24 19:20:50 +0100

    commit 24.11.24 19:20

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -1,4 +1,5 @@
 <?php
+    // Método post utilizado
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         $nom = $_POST['nom'];
         $edat = $_POST['edat'];
@@ -6,11 +7,16 @@
         $dades = $_POST['dades'];
     }
 
+    // Decodificando el JSON
     $dades_json = json_decode($dades, true);
 
     echo "Nombre: $nom <br>";
     echo "DNI: $dni <br>";
     echo "Edad: $edat <br>";
     echo json_encode($dades_json);
+
+    echo "Nombre: ".$dades_json['nom']."<br>";
+    echo "Edad: ".$dades_json['edat']."<br>";
+    echo "DNI: ".$dades_json['dni']."<br>";
 
 ?>
