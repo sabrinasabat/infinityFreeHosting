280f0942aef9a54f0ab86edeab836de95a9c1a29
sabrinasabat <sabrinasabats@gmail.com>
2024-11-24 21:05:05 +0100

    commit 24.11.24 21:04

---
+++ PHP/teste_inserirDades.php
@@ -0,0 +1,31 @@
+<?php
+    // URL do script principal
+    $url = "https://ssabat.infinityfreeapp.com/PHP/inserirDades.php";
+
+    // Dados a serem enviados no formato JSON
+    $dados = array(
+        "dades" => json_encode(array(
+            "nom" => "João Silva", // Nome
+            "edat" => 30,          // Idade
+            "dni" => "12345678A"   // DNI
+        ))
+    );
+
+    // Configurar a requisição POST
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($dados),
+        ),
+    );
+
+    $context = stream_context_create($options);
+
+    // Fazer a requisição para o script principal
+    $resultado = file_get_contents($url, false, $context);
+
+    // Exibir o resultado retornado pelo script principal
+    echo $resultado;
+?>
+
