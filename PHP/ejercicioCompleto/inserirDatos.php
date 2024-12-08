98817786dfe10142b4d64c58f7b4da2705daf977
sabrinasabat <sabrinasabats@gmail.com>
2024-12-08 19:43:01 +0100

    commit 08.12.24 19:42

--- PHP/ejercicioCompleto/inserirDatos.php
+++ PHP/ejercicioCompleto/inserirDatos.php
@@ -2,7 +2,7 @@
     include 'conexionBaseDeDatos.php';
     global $conexionDDBB;
 
-    $inserirAdm = "INSERT nombre, usuario, contrasena INTO usuarios VALUES ('Administrador', 'admin', 'admin')";
+    $inserirAdm = "INSERT INTO usuarios VALUES (nombre = 'Administrador', usuario = 'admin', contrasena = 'admin')";
 
     if($conexionDDBB->query($inserirAdm)===TRUE){
         echo 'Administrador creado e insertado con succeso.';
