7c5d746af06cd6b42c93cce96e1b56cbc3583725
sabrinasabat <sabrinasabats@gmail.com>
2024-12-09 11:22:39 +0100

    commit 09.12.24 11:22

--- PHP/examenTrimestral/borrar.php
+++ PHP/examenTrimestral/borrar.php
@@ -1,1 +1,13 @@
 <?php
+    include 'conexion.php';
+    global $conexion;
+
+    // borrar productos
+
+    $sql = "DELETE FROM productos WHERE id = 27";
+    if ($conexion->query($sql) === TRUE) {
+        echo "Producto borrado correctamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conexion->error;
+    }
+
