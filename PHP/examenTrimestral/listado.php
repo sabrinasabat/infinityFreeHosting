d2b099ebc69c00d5133cd8ffbe275eae557fe900
sabrinasabat <sabrinasabats@gmail.com>
2024-12-09 10:07:18 +0100

    commit 09.12.24 10:03

--- PHP/examenTrimestral/listado.php
+++ PHP/examenTrimestral/listado.php
@@ -7,9 +7,19 @@
 
     if($resultado->num_rows>0){
         while($arrayDeDatos = $resultado->fetch_assoc()){
+            echo '<table border="2px">';
             echo '<tr>';
+            echo '<th>Detalle</th>';
+            echo '<th>Código</th>';
+            echo '<th>Nombre</th>';
+            echo '<th>Acciones</th>';
+            echo '</tr>';
+            echo '<tr>';
+            echo '<td>Detalle</td>';
             echo '<td>'.$arrayDeDatos['id']. '</td>';
             echo '<td>'.$arrayDeDatos['nombre']. '</td>';
+            echo '<td>Acciones</td>';
             echo '</tr>';
+            echo '</table>';
         }
     }
