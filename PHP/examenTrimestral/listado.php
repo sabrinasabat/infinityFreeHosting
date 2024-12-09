1ee61723e9196c6c9d77692306cae15cd3ee7906
sabrinasabat <sabrinasabats@gmail.com>
2024-12-09 10:03:41 +0100

    commit 09.12.24 10:03

--- PHP/examenTrimestral/listado.php
+++ PHP/examenTrimestral/listado.php
@@ -1,1 +1,15 @@
 <?php
+    include 'conexion.php';
+    global $conexion;
+
+    $sql = "SELECT id, nombre FROM productos";
+    $resultado = $conexion -> query($sql);
+
+    if($resultado->num_rows>0){
+        while($arrayDeDatos = $resultado->fetch_assoc()){
+            echo '<tr>';
+            echo '<td>'.$arrayDeDatos['id']. '</td>';
+            echo '<td>'.$arrayDeDatos['nombre']. '</td>';
+            echo '</tr>';
+        }
+    }
