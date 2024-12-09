d505a5993c04ba68c613f91c9fd36240c947dcad
sabrinasabat <sabrinasabats@gmail.com>
2024-12-09 10:08:57 +0100

    commit 09.12.24 10:08

--- PHP/examenTrimestral/listado.php
+++ PHP/examenTrimestral/listado.php
@@ -6,20 +6,20 @@
     $resultado = $conexion -> query($sql);
 
     if($resultado->num_rows>0){
+        echo '<table border="2px">';
+        echo '<tr>';
+        echo '<th>Detalle</th>';
+        echo '<th>Código</th>';
+        echo '<th>Nombre</th>';
+        echo '<th>Acciones</th>';
+        echo '</tr>';
         while($arrayDeDatos = $resultado->fetch_assoc()){
-            echo '<table border="2px">';
-            echo '<tr>';
-            echo '<th>Detalle</th>';
-            echo '<th>Código</th>';
-            echo '<th>Nombre</th>';
-            echo '<th>Acciones</th>';
-            echo '</tr>';
             echo '<tr>';
             echo '<td>Detalle</td>';
             echo '<td>'.$arrayDeDatos['id']. '</td>';
             echo '<td>'.$arrayDeDatos['nombre']. '</td>';
             echo '<td>Acciones</td>';
             echo '</tr>';
-            echo '</table>';
         }
+        echo '</table>';
     }
