99e2deb95b782fe4109d61bb65dc12945535c2b3
sabrinasabat <sabrinasabats@gmail.com>
2025-01-13 20:22:39 +0100

    commit 13.01.25 20:22

--- PHP/testePeticion.php
+++ PHP/testePeticion.php
@@ -5,19 +5,24 @@ $metodo = $_SERVER["REQUEST_METHOD"];
 
 switch ($metodo){
     case "GET":
+        http_response_code(200); // ha funcionado
         echo json_encode(["mensaje" => "Hola. Hiciste una peticion GET"]);
         break;
     case "POST":
+        http_response_code(200);
         echo json_encode(["mensaje" => "Hola. Hiciste una peticion POST"]);
         break;
     case "PUT":
+        http_response_code(200);
         echo json_encode(["mensaje" => "Hola. Hiciste una peticion PUT"]);
         break;
     case "DELETE":
+        http_response_code(200);
         echo json_encode(["mensaje" => "Hola. Hiciste una peticion DELETE"]);
         break;
     default:
-        echo json_encode(["mensaje" => "Hola. Hiciste una peticion desconocida"]);
+        http_response_code(405); //no compatible con el servidor o el recurso
+        echo json_encode(["mensaje" => "Método no permitido"]);
         break;
 }
 ?>
